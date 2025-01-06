b7d8f501f2ec7a194d0361cff8ba4dc1d2d5dfae
jungyoonn <sophia7625@naver.com>
2025-01-06 16:50:44 +0900

    기본 틀

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Index from "./component/Index";
+import PostDetail from "./component/PostDetail";
+import Header from "./component/Header";
+import Posts from "./component/Posts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
