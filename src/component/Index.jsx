b9861eb57767908c69e9986867206ef295ffff8b
jungyoonn <sophia7625@naver.com>
2025-01-06 18:26:07 +0900

    인덱스 틀

--- src/component/Index.jsx
+++ src/component/Index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Index = () => {
   return (
     <div>
-      <h1>Index</h1>
+      <Container>
+        <h1>Index</h1>
+      </Container>
     </div>
   );
 }
